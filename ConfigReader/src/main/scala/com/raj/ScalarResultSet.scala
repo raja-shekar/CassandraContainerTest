a0686e3d0aba09cd59c57b76cdefaa20e9c62603
raja-shekar <rajashekar.siddu@glassbeam.com>
2021-09-03 18:21:07 +0530

    Updated test cases

--- ConfigReader/src/main/scala/com/raj/ScalarResultSet.scala
+++ ConfigReader/src/main/scala/com/raj/ScalarResultSet.scala
@@ -1,8 +1,85 @@
 package com.raj
 
-import com.datastax.oss.driver.api.core.cql.{ResultSet, Row}
+import com.datastax.oss.driver.api.core.{CqlIdentifier, ProtocolVersion}
+import com.datastax.oss.driver.api.core.`type`.DataType
+import com.datastax.oss.driver.api.core.`type`.codec.registry.CodecRegistry
+import com.datastax.oss.driver.api.core.cql.{ColumnDefinitions, ResultSet, Row}
+import com.datastax.oss.driver.api.core.detach.AttachmentPoint
 
-class ScalarResultSet(scalarResultSet: ResultSet) extends Iterator[Row]{
+import java.nio.ByteBuffer
+
+trait ScalarResultSet[T] extends Iterator[T]
+
+trait Mapper{
+  def mapper[T](f:ScalarRow => Option[T]):ScalarResultSet[T]
+}
+
+class ScalarResultSetWrapper(scalarResultSet: ResultSet) extends ScalarResultSet[ScalarRow] with Mapper {
+  self =>
   override def hasNext: Boolean = scalarResultSet.iterator().hasNext
-  override def next(): Row = scalarResultSet.iterator().next()
+
+  override def next(): ScalarRow = new ScalarRow(scalarResultSet.iterator().next())
+
+  override def mapper[T](f: ScalarRow => Option[T]): ScalarResultSet[T] = new ScalarResultSet[T]{
+    val newIt = self.flatMap(f)
+    override def hasNext: Boolean = newIt.hasNext
+    override def next(): T = newIt.next()
+  }
+}
+
+class ScalarRow(row: Row) extends Row{
+
+  override def getString(i: Int): String = super.getString(i)
+  override def getString(name: String): String = super.getString(name)
+
+  override def getInt(i: Int): Int = super.getInt(i)
+  override def getInt(name: String): Int = super.getInt(name)
+
+  override def getColumnDefinitions: ColumnDefinitions = {
+    row.getColumnDefinitions
+  }
+
+  override def firstIndexOf(name: String): Int = {
+    row.firstIndexOf(name)
+  }
+
+  override def getType(name: String): DataType = {
+    row.getType(name)
+  }
+
+  override def getBytesUnsafe(i: Int): ByteBuffer = {
+    row.getBytesUnsafe(i)
+  }
+
+  override def firstIndexOf(id: CqlIdentifier): Int = {
+    row.firstIndexOf(id)
+  }
+
+  override def getType(id: CqlIdentifier): DataType = {
+    row.getType(id)
+  }
+
+  override def size(): Int = {
+    row.size()
+  }
+
+  override def getType(i: Int): DataType = {
+    row.getType(i)
+  }
+
+  override def codecRegistry(): CodecRegistry = {
+    row.codecRegistry()
+  }
+
+  override def protocolVersion(): ProtocolVersion = {
+    row.protocolVersion()
+  }
+
+  override def isDetached: Boolean = {
+    row.isDetached
+  }
+
+  override def attach(attachmentPoint: AttachmentPoint): Unit = {
+    row.attach(attachmentPoint)
+  }
 }
--- ConfigReader/src/test/scala/CassandraSpec.scala
+++ ConfigReader/src/test/scala/CassandraSpec.scala
@@ -1,30 +1,98 @@
+
 import com.datastax.oss.driver.api.core.CqlSession
+import com.datastax.oss.driver.api.core.cql.SimpleStatement
 import com.datastax.oss.driver.internal.core.metadata.DefaultEndPoint
-import com.dimafeng.testcontainers.{CassandraContainer, ForAllTestContainer}
-import com.raj.ScalarResultSet
+import com.dimafeng.testcontainers.{CassandraContainer, Container, ForAllTestContainer}
+import com.raj.{ScalarResultSet, ScalarResultSetWrapper}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 import java.net.InetSocketAddress
 
-class CassandraSpec extends AnyFlatSpec with ForAllTestContainer with Matchers{
+case class Customer(id: Int, name: String)
+
+class CassandraRowSpec extends AnyFlatSpec with ForAllTestContainer with Matchers {
   override val container: CassandraContainer = CassandraContainer()
 
   container.start()
 
-  "Cassandra container" should "be started" in {
-    val session = CqlSession.builder()
-      .addContactEndPoint(new DefaultEndPoint(InetSocketAddress.createUnresolved(
-        container.cassandraContainer.getContainerIpAddress,
-        container.cassandraContainer.getFirstMappedPort.intValue()
-      ))).withLocalDatacenter("datacenter1").build()
+  val session = CqlSession.builder()
+    .addContactEndPoint(new DefaultEndPoint(InetSocketAddress.createUnresolved(
+      container.cassandraContainer.getContainerIpAddress,
+      container.cassandraContainer.getFirstMappedPort.intValue()
+    ))).withLocalDatacenter("datacenter1").build()
+  session.execute("CREATE KEYSPACE IF NOT EXISTS gb WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
+  session.execute("CREATE TABLE IF NOT EXISTS gb.test(id int, name text, PRIMARY KEY(id) )")
 
-    val rs = session.execute("select release_version from system.local")
-
-    val scalarRS = new ScalarResultSet(rs)
-    val row = scalarRS.next()
-
-    row.getString("release_version").length should be > 0
+  val stmt = session.prepare(s"INSERT INTO gb.test(id, name) VALUES(?,?)")
+  for (i <- 1 to 100) {
+    session.execute(stmt.bind().setInt("id", i).setString("name", s"test $i"))
   }
+
+  val s = SimpleStatement.newInstance("select id, name from gb.test").setPageSize(10)
+
+  "All Customers" should "be printed" in {
+    val result = new ScalarResultSetWrapper(session.execute(s))
+    val res: ScalarResultSet[Customer] = result.mapper(row => Option(Customer(row.getInt("id"), row.getString("name"))))
+    var counter = 0
+    while(res.hasNext) {
+      val customer = res.next()
+//      println(s"${customer.id} and name ${customer.name}")
+      counter += 1
+    }
+     counter should be >= 100
+  }
+
+  "Cassandra container" should "be started" in {
+    val result = new ScalarResultSetWrapper(session.execute(s))
+    var counter = 0
+    while (result.hasNext) {
+      result.next()
+      counter += 1
+    }
+    counter should be >= 100
+  }
+
 }
+//
+//class CassandraSpec extends AnyFlatSpec with ForAllTestContainer with Matchers {
+//  override val container: CassandraContainer = CassandraContainer()
+//
+//  container.start()
+//
+//  "Cassandra container" should "be started" in {
+//    val session = CqlSession.builder()
+//      .addContactEndPoint(new DefaultEndPoint(InetSocketAddress.createUnresolved(
+//        container.cassandraContainer.getContainerIpAddress,
+//        container.cassandraContainer.getFirstMappedPort.intValue()
+//      ))).withLocalDatacenter("datacenter1").build()
+//    session.execute("CREATE KEYSPACE IF NOT EXISTS gb WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
+//    session.execute("CREATE TABLE IF NOT EXISTS gb.test(id int, name text, PRIMARY KEY(id) )")
+//    val stmt = session.prepare(s"INSERT INTO gb.test(id, name) VALUES(?,?)")
+//
+//
+//    for (i <- 1 to 100) {
+//      session.execute(stmt.bind().setInt("id", i).setString("name", s"test $i"))
+//    }
+//
+//    val s = SimpleStatement.newInstance("select id, name from gb.test").setPageSize(10)
+//
+//    val result = new ScalarResultSetWrapper(session.execute(s))
+//    var counter = 0
+//    while (result.hasNext) {
+//      result.next()
+//      counter += 1
+//    }
+//    counter should be >= 100
+//
+//    //    val rs = session.execute("select release_version from system.local")
+//    //
+//    //    val scalarRS = new ScalarResultSetWrapper(rs)
+//    //    val row = scalarRS.next()
+//    //    val test = scalarRS.mapper(row => Option(Version(row.getString("release_version"))))
+//    //    test.next().version
+//    //    row.getString(1)
+//    //    row.getString("release_version").length should be > 0
+//  }
+//}
